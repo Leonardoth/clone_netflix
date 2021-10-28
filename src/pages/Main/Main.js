bf8e239223526004787db4da2d4bf6fd107d140a
lowqualityplays <lqplays@outlook.com>
2021-10-27 21:06:46 -0300

    Updated Main page

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './index.scss';
 
-// Titles
-import lists from '../../utilities/lists';
-
 // Components
 import Banner from '../../components/Banner/Banner';
 import List from '../../components/List/List';
 
+// Data
+import { lists, banners } from '../../utilities/data';
+
 function Main() {
+  function getBanner() {
+    let max = banners.length;
+    let index = Math.floor(Math.random() * max);
+    return banners[index];
+  }
+
   return (
-    <div>
-      <Banner />
+    <div className='Main'>
+      <Banner item={getBanner()} />
       {lists.map(item => {
         return <List title={item.title} items={item.list} />;
       })}
