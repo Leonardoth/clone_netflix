5a76c6de0adc61f5fe57f0e249ef754b9bb6483e
lowqualityplays <lqplays@outlook.com>
2021-11-04 21:27:27 -0300

    Removed overflow-x from App

--- src/utilities/data.js
+++ src/utilities/data.js
@@ -85,7 +85,7 @@ const lists = [
 
 const banners = [
   {
-    tags: ['Cativante', 'Divertido', 'Engraçado', 'Infantil'],
+    tags: ['Cativante', 'Divertido', 'Engraçado', 'Infantil', 'Otro'],
     image:
       'https://images.ctfassets.net/4cd45et68cgf/3BJQrujAXLeXXsWXnolmvB/28845a87ecb25a0609c2e9faec57736c/EN_SQdGame_Main_PlayGround_Horizontal_RGB_PRE.jpg',
     mobileImage:
